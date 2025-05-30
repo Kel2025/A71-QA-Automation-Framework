a1f727877481085985bce86dd28995993129ba41
Kel2025 <kelly.wade@testpro.io>
2025-05-29 17:31:34 -0700

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -19,7 +19,7 @@ public class LoginTests extends BaseTest {
 
         // TODO: Fix the URL if the test fails
         // First Test
-        String url = "https://testpro.io/";
+        String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
