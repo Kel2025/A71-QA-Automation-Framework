12f9f65f81f3eaeacf65096bb1cfe584ca8439b0
Kel2025 <kelly.wade@testpro.io>
2025-06-04 12:52:24 -0700

    Fixed browser issue

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -37,37 +37,3 @@ public class Homework16 extends BaseTest {
     }
 }
 
-
-//        Assert.assertEquals(driver.getCurrentUrl(), url);
-//        driver.quit();
-//    }
-//}
-
-
-
-//public class Homework16 extends BaseTest {
-//
-//  //   @Test
-//    public void registrationNavigation() {
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--remote-allow-origins=*");
-//
-//        WebDriver driver = new ChromeDriver(options);
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//
-//        //navigate to url
-//        String url = "https://qa.koel.app/";
-//        driver.get(url);
-//
-//
-//        // Click registration link
-//       WebElement registrationLink = driver.findElement(By.cssSelector("a[href^=\"registration\"]"));
-//        registrationLink.click();
-//        //Verify that you are redirected to Registration page using Assert method
-//        WebElement registrationPage = driver.findElement(By.cssSelector("span[class=\"small\"]"));
-//        Assert.assertTrue(registrationPage.isDisplayed());
-//        driver.quit();
-//    }
-//}
-//
-//
