5cc962f9b4e63e0ffb27d12993e42c8c3cc67464
Kel2025 <kelly.wade@testpro.io>
2025-06-04 12:43:27 -0700

    Fixed browser issue
    Fixed browser issue

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -5,31 +5,69 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import java.time.Duration;
 
-
 public class Homework16 extends BaseTest {
-
     @Test
     public void registrationNavigation() {
+
+        //Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
-        WebDriver driver = new ChromeDriver();
+        //open browser
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        //navigate to url
+        //navigate to koel app url
         String url = "https://qa.koel.app/";
         driver.get(url);
 
-        // Click registration link
+        //Click registration link
         WebElement registrationLink = driver.findElement(By.cssSelector("a[href^=\"registration\"]"));
         registrationLink.click();
-        //Verify that you are redirected to Registration page using Assert method
+
+        //Verify redirection to Registration page using Assert method
         WebElement registrationPage = driver.findElement(By.cssSelector("span[class=\"small\"]"));
         Assert.assertTrue(registrationPage.isDisplayed());
+
+        //close browser
         driver.quit();
     }
 }
 
 
+//        Assert.assertEquals(driver.getCurrentUrl(), url);
+//        driver.quit();
+//    }
+//}
+
+
+
+//public class Homework16 extends BaseTest {
+//
+//  //   @Test
+//    public void registrationNavigation() {
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--remote-allow-origins=*");
+//
+//        WebDriver driver = new ChromeDriver(options);
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//
+//        //navigate to url
+//        String url = "https://qa.koel.app/";
+//        driver.get(url);
+//
+//
+//        // Click registration link
+//       WebElement registrationLink = driver.findElement(By.cssSelector("a[href^=\"registration\"]"));
+//        registrationLink.click();
+//        //Verify that you are redirected to Registration page using Assert method
+//        WebElement registrationPage = driver.findElement(By.cssSelector("span[class=\"small\"]"));
+//        Assert.assertTrue(registrationPage.isDisplayed());
+//        driver.quit();
+//    }
+//}
+//
+//
