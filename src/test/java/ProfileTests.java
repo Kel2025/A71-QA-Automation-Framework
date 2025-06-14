f442c88b6a628689e2fb2e4cb6894088a550b31f
Kel2025 <kelly.wade@testpro.io>
2025-06-14 15:29:41 -0700

    Update profiles tests with waits

--- src/test/java/ProfileTests.java
+++ src/test/java/ProfileTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 
 import java.util.UUID;
 
+
 public class ProfileTests extends BaseTest {
 
     @Test
@@ -13,39 +14,27 @@ public class ProfileTests extends BaseTest {
 
         //Navigate to Koel app
         navigateUrl();
-
         //provide email
         provideEmail("kelly.Wade@testpro.io");
-
         //provide password
         providePassword("P!990109189300ok");
-
         //click submit
         clickLoginBtn();
         String randomName = randomName();
-
         //pause for 2 seconds for page to load
         //Thread.sleep(2000);
-
         //click avatar icon to go to profile setting page
-        clickProfileName();
-
+        clickAvatarIcon();
         //provide current Password
         provideCurrentPassword("P!990109189300ok");
-
         //provide profile name
         provideProfileName(randomName);
-
         //click save button
         clickSaveButton();
-
         //wait 2 seconds
         //Thread.sleep(2000);
-
         //verify profile name change
-        //WebElement actualProfileName = driver.findElement(By.cssSelector("span[class='name']"));
-        WebElement actualProfileName = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[class='name']")));
+        WebElement actualProfileName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[class='name']")));
         Assert.assertEquals(actualProfileName.getText(),randomName);
     }
 
@@ -53,27 +42,30 @@ public class ProfileTests extends BaseTest {
         return UUID.randomUUID().toString().replace("-","");
     }
 
-    public void clickProfileName(){
+    public void clickAvatarIcon(){
         //WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
-        WebElement profileName = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[class='name']")));
-        profileName.click();
+        WebElement avatarIcon = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar")));
+        avatarIcon.click();
     }
 
     public void provideCurrentPassword(String Password) {
-        WebElement currentPassword = driver.findElement(By.cssSelector("[name='current_password']"));
+        WebElement currentPassword = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name='current_password']")));
         currentPassword.sendKeys(Password);
     }
 
     public void provideProfileName(String randomName) {
-        WebElement profileNameField = driver.findElement(By.cssSelector("[name='name']"));
+        WebElement profileNameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
         profileNameField.clear();
         profileNameField.sendKeys(randomName);
     }
 
     public void clickSaveButton() {
-        WebElement saveButton = driver.findElement(By.cssSelector("button.btn-submit"));
+        WebElement saveButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.btn-submit")));
         saveButton.click();
     }
 
 }
+
+
+
