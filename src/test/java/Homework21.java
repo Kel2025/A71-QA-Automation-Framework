b51df1f0eaaba88b48ae9776c3073d5c8ecc72f7
Kel2025 <kelly.wade@testpro.io>
2025-06-19 22:08:20 -0700

    Updated Homework21
    fixed issue with actions not working

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -1,22 +1,34 @@
-import com.github.dockerjava.core.KeystoreSSLConfig;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 
 public class Homework21 extends BaseTest {
+    Actions actions = new Actions(driver);
 
+    @BeforeMethod
+    public void setUp() {
+        //Initialize the WebDriver and other necessary components
+        driver = new ChromeDriver();
+        actions = new Actions(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
 
     @Test
     // prerequisite: must have at least 1 user playlist created
     public void renamePlaylist() {
         String newPlaylistName = generateRandomPlaylistName();
-        Actions actions = new Actions(driver);
+
         login();
 
         // double click playlist from side menu
@@ -26,12 +38,8 @@ public class Homework21 extends BaseTest {
         enterPlaylistName(newPlaylistName);
 
         //verify success message and playlist name is changed
-        WebElement successNotification = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
-        WebElement playlist = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(
-                        By.cssSelector(".playlist:nth-child(3)")));
-        Assert.assertEquals(playlist.getText(), newPlaylistName );
+        Assert.assertTrue(successNotification.isDisplayed());
+        Assert.assertEquals(playlist.getText(), newPlaylistName);
     }
 
     // generate random name for playlist
@@ -61,6 +69,15 @@ public class Homework21 extends BaseTest {
         playlistNameField.sendKeys(newPlaylistName);
         playlistNameField.sendKeys(Keys.ENTER);
     }
+    //web element locator and wait for success notification
+    WebElement successNotification = wait.until(
+            ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+
+    // web element, locator, and wait for playlist to assert name change
+    WebElement playlist = wait.until(
+            ExpectedConditions.visibilityOfElementLocated(
+                    By.cssSelector(".playlist:nth-child(3)")));
+
 }
 
 
